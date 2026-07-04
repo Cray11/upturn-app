854dbea166d892b519771b3377cf75417a244c69
Cray11 <151000886+Cray11@users.noreply.github.com>
2026-07-04 15:39:36 +0800

    Fix vercel.json routes and api/index.php

--- api/index.php
+++ api/index.php
@@ -1,21 +1,17 @@
 <?php
 
-declare(strict_types=1);
+use Illuminate\Http\Request;
 
-// Set Content-Type for all responses
-header('Content-Type: text/html; charset=UTF-8');
+define('LARAVEL_START', microtime(true));
 
-// Boot Laravel
-require __DIR__ . '/../vendor/autoload.php';
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
